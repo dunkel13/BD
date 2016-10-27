d19b9bd3d2eeedd49085d20478d325865d04f031
dunkel13 <fedom9@gmail.com>
2016-10-27 17:40:42 -0500

    Delete vw_obra2.sql

--- vw_obra2.sql
+++
@@ -1,6 +0,0 @@
-create view vw_obra2 as select * from obra;
-select * from vw_obra2;
-update vw_obra2 set obr_costo=1500 where obr_nombre_obra='Mona Lisa';
-set SQL_SAFE_UPDATES=0;
-
-update vw_obra2 set obr_tipo_obra='Boceto' where  obr_nombre_obra='Mona Lisa';
