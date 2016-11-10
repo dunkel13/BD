f98610e487a71423c916371166e62ed4c904dcf4
dunkel13 <fedom9@gmail.com>
2016-11-10 06:39:36 -0500

    Update triggers_08_nov.sql

--- triggers_08_nov.sql
+++ triggers_08_nov.sql
@@ -1,14 +1,4 @@
 # borrado en cascada, al borrar un autor deben borrarse los libros que escribio de la tabla libro
-drop trigger if exists tr_1;
-delimiter $$
-create trigger tr_1 after delete on autor
-for each row begin
-insert into historia values(OLD.lib_titulo, OLD.AUT_nombre, curdate());
-delete from libro where 
-end$$
-
-delimiter ; 
-
 drop trigger if exists tr_2;
 delimiter $$
 create trigger tr_2 after update on libro
@@ -21,3 +11,31 @@ delimiter ;
 UPDATE libro set lib_precio=24000 where lib_id=8;
 UPDATE libro set lib_precio=48000 where lib_id=8;
 select * from bitacora;
+
+drop trigger if exists borrado_1;
+drop trigger if exists borrado_2;
+
+delimiter $$
+create trigger borrado_2 before delete on autor
+FOR EACH ROW BEGIN
+SET @id=old.AUT_id;
+SELECT AUT_nombre into @nom FROM libro_has_autor JOIN autor ON AUTOR_AUT_id=AUT_id where AUTOR_AUT_id=@id;
+SELECT lib_titulo into @title FROM libro_has_autor JOIN libro ON lib_id=LIBRO_lib_id where AUTOR_AUT_id=@id;
+INSERT INTO historia(HIS_lib_titulo,HIS_lib_AUT_nombre,HIS_fecha) VALUES(@title,@nom,curdate()); 
+DELETE FROM libro_has_autor WHERE AUTOR_AUT_id=@id;
+
+end; $$
+delimiter ;
+
+
+delimiter $$
+CREATE TRIGGER borrado_1 AFTER DELETE ON libro_has_autor
+FOR EACH ROW BEGIN
+set @lib=old.LIBRO_lib_id;
+DELETE FROM editorial WHERE id_libro=@lib;
+DELETE FROM libro WHERE lib_id=@lib;
+end; $$
+delimiter ;
+
+DELETE FROM autor WHERE AUT_id = 1345;
+SELECT * FROM historia;
